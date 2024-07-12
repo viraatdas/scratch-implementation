9b536845d20c2792cade0589e7c01efc38930bc4
Viraat Das <viraat.laldas@gmail.com>
2024-07-11 19:36:28 -0700

    decoder

--- gpt/model.py
+++ gpt/model.py
@@ -110,3 +110,45 @@ class Encoder(nn.Module):
     return self.norm(x)
 
 
+class DecoderBlock(nn.Module):
+  def __init__(self, features: int, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+    super().__init__()
+    self.self_attention_block = self_attention_block
+    self.cross_attention_block = cross_attention_block
+    self.feed_forward_block = feed_forward_block
+    self.residual_connections = nn.ModuleList([ResidualConnection(features, dropout) for _ in range(3)])
+    self.norm = nn.LayerNorm(features)
+
+  """
+  src_mask: mask the input source sequence 
+  tgt_mask: used in the decoder to mask out padding tokens 
+            to prevent them from seeing the target sequence
+  """
+  def forward(self, x, encoder_output, src_mask, tgt_mask):
+    x = self.residual_connections[0](x, lambda x: self.self_attention_block(x,x,x, tgt_mask))
+    x = self.residual_connections[1](x, lambda x: self.self_attention_block(x,encoder_output, encoder_output, src_mask))
+    x = self.residual_connections[2](x, lambda x: self.feed_forward_block(x))
+
+
+class DecoderBlock(nn.Module):
+  def __init__(self, features: int, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+    super().__init__()
+    self.self_attention_block = self_attention_block
+    self.cross_attention_block = cross_attention_block
+    self.feed_forward_block = feed_forward_block
+    self.residual_connections = nn.ModuleList([ResidualConnection(features, dropout) for _ in range(3)])
+    self.norm = nn.LayerNorm(features)
+
+  """
+  src_mask: mask the input source sequence 
+  tgt_mask: used in the decoder to mask out padding tokens 
+            to prevent them from seeing the target sequence
+  """
+  def forward(self, x, encoder_output, src_mask, tgt_mask):
+    x = self.residual_connections[0](x, lambda x: self.self_attention_block(x,x,x, tgt_mask))
+    x = self.residual_connections[1](x, lambda x: self.self_attention_block(x,encoder_output, encoder_output, src_mask))
+    x = self.residual_connections[2](x, lambda x: self.feed_forward_block(x))
+
+
+         
+
